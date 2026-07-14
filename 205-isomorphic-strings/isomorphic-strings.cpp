7c8a543df888b6920899ef8899fd6da9e62398a2
rachnaa-s <rachnasurjaye25@gmail.com>
2026-07-14 22:50:11 +0530

    Time: 5 ms (11.01%) | Memory: 9.4 MB (29.23%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char , char>mp;
+        unordered_map<char , char>mp2;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(mp.count(s[i])){
+                if(mp[s[i]]!=t[i]){
+                    return false;
+                }
+            }
+            else{
+                mp[s[i]]=t[i];
+            }
+
+            if(mp2.count(t[i])){
+                if(mp2[t[i]]!=s[i]){
+                    return false;
+                }
+            }
+            else{
+                mp2[t[i]]=s[i];
+            }
+        }
+        return true;
+        
+    }
+};
